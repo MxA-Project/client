95366a4f914a7a1410d8fa8a88d37ef54e08beec
Wakedel <nd.alexandre.mansire@gmail.com>
2018-08-03 19:00:56 +0200

    version 0.1 works properly to do : manage differents issues and interaction with user

--- Compteur.ino
+++ Compteur.ino
@@ -1,93 +1,195 @@
-//Libraries used to display
-#include <MD_MAX72xx.h>
+//################# LIBRARIES ##########################
 #include <SPI.h>
-
-//Libraries used to get data
+#include <Adafruit_GFX.h>
+#include <Max72xxPanel.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-// Define the number of devices we have in the chain and the hardware interface
-// NOTE: These pin numbers will probably not work with your hardware and may
-// need to be adapted
-#define HARDWARE_TYPE MD_MAX72XX::PAROLA_HW  //Choisir en fonction du matériel utilisé https://majicdesigns.github.io/MD_MAX72XX/class_m_d___m_a_x72_x_x.html#a88ea7aada207c02282d091b7be7084e6
-#define MAX_DEVICES 5
+//################# Settings ##########################
+#define ssid "SFR_9C08"
+#define password "cerolutenocks7arally"
 
-#define CLK_PIN   13  // or SCK
-#define DATA_PIN  11  // or MOSI
-#define CS_PIN 10 // or SS
+#define serverUrl "http://requestbin.net/r/1iqgqlx1"
 
-#define MAX_NB_CHAR 5
+#define interval 30000 // soit 30 secondes            // interval between data requests in milliseconds
+#define millisMax 86400000                            // maximum interval scince system start
 
-String message
-
-// SPI hardware interface
-MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
-// Arbitrary pins
-//MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
-
-// Text parameters
-#define CHAR_SPACING 1 // pixels between characters
+#define refreshMax 60 // = 1,8e+6 / interval (soit toutes les 30 minutes)
 
 
-// WiFi login parameters - network name and password
-#define ssid ""
-#define password ""
+//################# Other variables ##########################
 
-// Server connection settings
-#define serverUrl ""
+// Variables used by the display
+int const pinCS = D4; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
+int const numberOfHorizontalDisplays = 4;
+int const numberOfVerticalDisplays   = 1;
+String message;
+
+Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+int const wait   = 300;               // In milliseconds between scroll movements
+int const spacer = 1;                 // space between two char
+int const width  = 5 + spacer;        // The font width is 5 pixels
 
 
+// Variables used for control
+long previousMillis = 0;          // will store last time Message was updated
+//const int interval = 30000;          // interval between data requests in milliseconds
+//const int intervalMax = 864;         // maximum interval scince system start
+int failCount = 0;                // will store number of time the server didn't answer
+int refreshCount = 0;             // will store number of time 'message' variable was refresh
 
-void GetServerMessage() {
-    HTTPClient http;  //Declare an object of class HTTPClient
-    http.begin(serverUrl);
-    int httpCode = http.GET();
-    if (httpCode == 200) { // if all is right, then we update message variable
-      message = http.getString();
-      Serial.println(message);
-    }
-    http.end();
+//################# Functions ##########################
+
+// Print a string on the display
+void display_message(String message){
+  int ind = matrix.width() / width - message.length();
+  if (ind >= 0)
+  {
+    display_message_without_scroll(message);
+  }
+  else
+  {
+    display_message_with_scroll(message);
+  }
 }
 
-/*
--Preparer le buffer à partir du message à afficher
-exemple 'uint8_t globalBuf[2*(8*MAX_DEVICES)]' : il faut définir sa taille directement car pas pratique dynamiquement quand peu d'espace
+// Display a message using scrolling
+void display_message_with_scroll(String message){
+   for ( int i = 0 ; i < width * message.length() + matrix.width() - spacer; i++ ) {
+    //matrix.fillScreen(LOW);
 
-on insère x premières colonnes vides dans le buffer
-pour chaque caractère du texte :
-  on charge le caractère dans un buffer temp
-  on copie le contenu du buffer à la suite des infos précédentes dans le buffer global
-  on insère x colonne vide d'espace avant le prochain caractère
+    //width  = 5 + spacer; // The font width is 5 pixels
 
--Affiche le texte
-si message plus long que place dispo : on fait dérouler le texte
-  affichage ça défile vers la gauche une fois en entier, puis après petit temps ça recommence
-si message plus court on centre le texte à droite ou à gauche (paramètre à définir)
-*/
+    int letter = i / width;
+    int x = (matrix.width() - 1) - i % width;
+    //int y = (matrix.height() - 8) / 2; // center the text vertically if more than 1 matrix vertically else y=0
+    while ( x + width - spacer >= 0 && letter >= 0 ) {
+      if ( letter < message.length() ) {
+        matrix.drawChar(x, 0 /*or 'y' */, message[letter], HIGH, LOW, 1); // HIGH LOW means foreground ON, background OFF, reverse these to invert the display!
+      }
+      letter--;
+      x -= width;
+    }
+    matrix.write(); // Send bitmap to display
+    delay(wait/2);
+  }
+}
 
+void display_message_without_scroll(String message){
+  int x = matrix.width();
+  for ( int i = message.length()-1 ; i>=0; i-- ) { //tester peut être décalage de 1 ???
+    matrix.drawChar(x - width, 0, message[i], HIGH, LOW, 1);
+    x -= width;
+  }
+  matrix.write();
+}
+
+
+// Get the number from the server
+void GetServerMessage(){
+    if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+
+      Serial.println(F("Try to connect server"));
+
+      HTTPClient http;  //Declare an object of class HTTPClient
+
+      http.begin(serverUrl);  //Specify request destination
+      int httpCode = http.GET();                       //Send the request
+
+      Serial.println(httpCode);
+
+      if (httpCode > 0) { //Check the returning code
+        Serial.println(F("Data received"));
+
+        message = http.getString();   //Get the request response message
+        Serial.println(message);             //Print the response message
+        } else {
+          // Server didn't respond properly
+          Serial.println(F("Server didn't respond properly"));
+          //faire qqc jsp quoi led ou message
+        }
+    http.end();   //Close connection
+
+  } else {
+    // If wifi connection is down
+    Serial.println(F("Wifi connection down"));
+    //faire qqc
+    while (true){
+      display_message("Votre connection wifi semble défaillante : vérifiez la et redémarrer l'afficheur");
+    }
+  }
+}
+
+// Declare reset function at adress 0
+void(* resetFunc) (void) = 0;
+
+
+//################# Program ##########################
 void setup() {
-  message = "error"
+  // Start connection with pc
+  Serial.begin(115200); // initialize serial communications
+  Serial.println(F("Lancement"));
 
-  // initialize serial communications
-  Serial.begin(115200);
+  // Set up display
+  matrix.setIntensity(2);    // Use a value between 0 and 15 for brightness
+  matrix.setRotation(0, 1);  // The first display is position upside down
+  matrix.setRotation(1, 1);  // The first display is position upside down
+  matrix.setRotation(2, 1);  // The first display is position upside down
+  matrix.setRotation(3, 1);  // The first display is position upside down
 
-  // initialize wifi connection
-  WiFiManager wifiManager;
-  wifiManager.setTimeout(180);
+  //
+  message = "...";              // écrire un message qui s'affiche au lancement
+  display_message(message);
+  Serial.println(message);
 
-  if(!wifiManager.autoConnect(ssid, password)) {
-    Serial.println(F("failed to connect and timeout occurred"));
-    delay(6000);
-    ESP.reset(); //reset and try again
-    delay(180000);
+  // Set up wifi connection
+  WiFi.begin(ssid, password);
+  // The client will try only 20 times to connect to wifi
+  int count = 0;
+  while (WiFi.status() != WL_CONNECTED || count == 20) { // '||' c'est un 'ou'
+    count++;
+    delay(1000);
+    Serial.println(F("Connecting.."));
   }
 
-  // initialize display
-  mx.begin();
-
+  GetServerMessage();
 }
 
-void loop() {
-  GetServerMessage()
-  printText(0, MAX_DEVICES-1, message);
+
+void loop(){
+  display_message(message); // Display the message
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis;
+    GetServerMessage();
+    refreshCount++;
+
+    // We will not test the following condition each time that's why we used a counter
+    if (refreshCount == refreshMax){
+      refreshCount = 0;
+      // In order to limit the memory usage to store the time data, we periodically restart the card
+      if (currentMillis >= millisMax){
+        resetFunc();
+      }
+    }
+  }
+
+
+/*
+à faire
+
+on vérifie tous les x requêtes au serv :
+  si temps max atteint : on redémarre le système
+
+  où x = 1,8e+6 / interval (soit toutes les 30 minutes
+OK
+à refaire je suis trop con
+faut juste remplacer le test par un ajustement du compteur en changeant le max
+
+si nombre max d'échecs atteint, on affiche un message pour dire de nous contacter
+
+si co wifi pas bonne afficher un message
+OK (peut être revoir en fonction de ce qu'on veut)
+ */
+
 }
